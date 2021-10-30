2cc61a2633e6d95ac45de26a76e051bf678662da
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-10-30 23:00:38 +0200

    frontend done

--- src/views/Reserve.vue
+++ src/views/Reserve.vue
@@ -22,7 +22,7 @@
           <input type="text" id="adresse" required>
         </p> 
       </div>
-      <div class="reserve_city">
+      <div class="reserve_ville">
         <span>Code postal : 
           <input type="text" id="code_postal" required>
         </span>
@@ -31,7 +31,7 @@
         </span>
       </div>
       <div class="button">
-        <button>Confirmer</button>
+        <button @click="goToServices()">Confirmer</button>
       </div>
     </div>
   </div>
@@ -48,6 +48,11 @@ export default {
       idElem: this.$route.params.id,
     };
   },
+  methods:{
+    goToServices() {
+      this.$router.push("/services");
+    },
+  }
 };
 </script>
 
@@ -63,7 +68,7 @@ export default {
 
 .reserve_wrapper{
   height: 80%;
-  width: 80%;
+  width: 50%;
 }
 
 .reserve_wrapper h1{
@@ -73,6 +78,72 @@ export default {
   letter-spacing: 0.7px;
   color: #1C4A15;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+}
+
+.reserve_choice{
+  margin-top: 10%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.reserve_adresse{
+  margin-top: 10%;
+}
+
+#adresse{
+  width: 30%;
+}
+
+.reserve_ville{
+  margin-top: 6%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.button{
+  margin-top: 10%
+}
+
+.button button {
+  cursor: pointer;
+  padding: 20px 40px;
+  background: #e9e3d1;
+  border: none;
+  border-radius: 10px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  color: #1c4a15;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+  transition: all 0.3s ease;
+}
+
+.button button:hover {
+  transform: scale(1.03);
+}
+
+input {
+  padding: 0 0 0 6px; /*0 0 5px 6px*/ 
+  outline: 0;
+  border-width: 0 0 2px;
+  border-color: #83735b;
+  background: none;
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  letter-spacing: 0.7px;
+  color: #7a7663;
+}
+
+span, p{
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 40px;
+  color: #7a7663;
 }
 
 
